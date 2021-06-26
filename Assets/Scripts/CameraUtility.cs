cbc9c8d7427d303eb8aa578690c1ed261bcceaae
yoonwoo <as04107@gmail.com>
2021-06-27 02:03:12 +0900

    카메라 유틸 클래스 추가

---
+++ Assets/Scripts/CameraUtility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CameraUtility
+{
+    public static Vector2 ToScreenPos(this Camera target, Vector2 screenSize, Vector2 position)
+    {
+        var viewportPoint = target.ScreenToViewportPoint(position) - new Vector3(0.5f, 0.5f);
+        var worldPoint = new Vector2(viewportPoint.x * screenSize.x, viewportPoint.y * screenSize.y);
+        return worldPoint;
+    }
+}
